06f58b8384dcfbf7ee15c45fd907352b0ef65403
corona9955 <daniel.corona193@tectijuana.edu.mx>
2023-06-15 10:12:20 -0700

    Detección de puerto.

--- Form1.cs
+++ Form1.cs
@@ -13,31 +13,42 @@ namespace ConexionDePuertos
 {
     public partial class Form1 : Form
     {
-        //SerialPort serialPort;
+        SerialPort serialPort;
         public Form1()
         {
             InitializeComponent();
-            //SerialPort serialPort = new SerialPort("COM1", 9600); // Reemplaza "COMX" con el nombre del puerto COM correcto
-            //serialPort.DataReceived += SerialPort_DataReceived;
-            //serialPort.Open();
+            serialPort = new SerialPort("COM3", 9600); // Configura el puerto COM y la velocidad de baudios
+            serialPort.DataReceived += SerialPort_DataReceived; // Asigna el evento DataReceived
+
+            try
+            {
+                serialPort.Open(); // Abre el puerto serial
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el puerto serial: " + ex.Message);
+            }
             LoadAvailablePorts();
         }
-        //private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
-        //{
-        //    string data = serialPort.ReadLine(); // Lee una línea de datos desde el puerto COM
-        //    // Realiza el procesamiento necesario con los datos recibidos
-        //    // Aquí, simplemente actualizaremos el cuadro de texto con los datos recibidos
-        //    Invoke(new Action(() =>
-        //    {
-        //        txtData1.AppendText(data + Environment.NewLine);
-        //    }));
-        //}
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Se ejecuta cuando se recibe un dato en el puerto serial
+            string data = serialPort.ReadExisting(); // Lee los datos recibidos
 
-        //private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        //{
-        //    if (serialPort.IsOpen)
-        //        serialPort.Close();
-        //}
+            // Realiza el procesamiento de los datos recibidos como desees
+            // Puedes actualizar los controles de la interfaz de usuario, guardar los datos, etc.
+
+            // Ejemplo: Mostrar los datos en un TextBox llamado "textBoxData"
+            txtData1.Invoke((MethodInvoker)(() => txtData1.AppendText(data)));
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort != null && serialPort.IsOpen)
+            {
+                serialPort.Close(); // Cierra el puerto serial antes de salir de la aplicación
+            }
+        }
+
 
         private void LoadAvailablePorts()
         {
