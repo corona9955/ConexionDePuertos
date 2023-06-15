45083558b2dba499e457f1b0bca111aab2c6e6a5
corona9955 <daniel.corona193@tectijuana.edu.mx>
2023-06-15 09:47:33 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace ConexionDePuertos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtData1 = new System.Windows.Forms.TextBox();
+            this.cmbxPuertos = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // txtData1
+            // 
+            this.txtData1.Location = new System.Drawing.Point(86, 146);
+            this.txtData1.Name = "txtData1";
+            this.txtData1.Size = new System.Drawing.Size(368, 20);
+            this.txtData1.TabIndex = 0;
+            // 
+            // cmbxPuertos
+            // 
+            this.cmbxPuertos.FormattingEnabled = true;
+            this.cmbxPuertos.Location = new System.Drawing.Point(86, 64);
+            this.cmbxPuertos.Name = "cmbxPuertos";
+            this.cmbxPuertos.Size = new System.Drawing.Size(196, 21);
+            this.cmbxPuertos.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbxPuertos);
+            this.Controls.Add(this.txtData1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtData1;
+        private System.Windows.Forms.ComboBox cmbxPuertos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace ConexionDePuertos
+{
+    public partial class Form1 : Form
+    {
+        //SerialPort serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+            //SerialPort serialPort = new SerialPort("COM1", 9600); // Reemplaza "COMX" con el nombre del puerto COM correcto
+            //serialPort.DataReceived += SerialPort_DataReceived;
+            //serialPort.Open();
+            LoadAvailablePorts();
+        }
+        //private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        //{
+        //    string data = serialPort.ReadLine(); // Lee una línea de datos desde el puerto COM
+        //    // Realiza el procesamiento necesario con los datos recibidos
+        //    // Aquí, simplemente actualizaremos el cuadro de texto con los datos recibidos
+        //    Invoke(new Action(() =>
+        //    {
+        //        txtData1.AppendText(data + Environment.NewLine);
+        //    }));
+        //}
+
+        //private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        //{
+        //    if (serialPort.IsOpen)
+        //        serialPort.Close();
+        //}
+
+        private void LoadAvailablePorts()
+        {
+            string[] portNames = SerialPort.GetPortNames();
+            cmbxPuertos.DataSource = portNames;
+        }
+    }
+}
